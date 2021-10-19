44a5aa3b95090636f13151d7ce48b1ea1e88861b
zaki88-ahmed <zaki.ahmed88@gmail.com>
2021-10-19 11:32:28 +0200

    after_user_crud_v1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,18 @@
 
 namespace App\Exceptions;
 
+use App\Http\Traits\ApiDesignTrait;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiDesignTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -38,4 +45,21 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ValidationException) {
+            return $this->ApiResponse(400, "Validation Errors", $e->errors());
+        }elseif ($e instanceof NotFoundHttpException){
+            return $this->ApiResponse(400, "Validation Errors", $request->url(), 'Not Found, try with correct url');
+        }elseif ($e instanceof AuthorizationException || $e instanceof AccessDeniedHttpException){
+            return $this->ApiResponse(400, "unauthorized");
+        }elseif ($e instanceof AuthenticationException){
+            return $this->ApiResponse(400, "unauthenticated");
+        }
+        else{
+            return $this->ApiResponse(400, "Errors", $e->getMessage());
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/users/register',  [AuthController::class, 'register']);
+Route::post('/users/login',  [AuthController::class, 'login']);
+Route::post('/users/logout',  [AuthController::class, 'logout']);
+Route::post('/users/refresh',  [AuthController::class, 'refresh']);
+Route::post('/users/me',  [AuthController::class, 'me']);
+Route::post('/users/update-password',  [AuthController::class, 'updatePassword']);
+Route::get('/users/all-users',  [AuthController::class, 'allUsers']);
+Route::get('/users/show',  [AuthController::class, 'userDetails']);
+Route::get('/users/show-by-id',  [AuthController::class, 'userDetailsById']);
+Route::post('/users/edit-by-id',  [AuthController::class, 'UserUpdateById']);
+Route::post('/users/edit',  [AuthController::class, 'updateUser']);
+Route::post('/users/delete',  [AuthController::class, 'softDeleteUser']);
+Route::post('/users/restore',  [AuthController::class, 'restoreUser']);
+Route::post('/users/refresh',  [AuthController::class, 'refresh']);
